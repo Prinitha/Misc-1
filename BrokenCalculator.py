ac6af7db1f19da3cf7c24797a43e3104da973dcd
Prini <nprinitha5@gmail.com>
2024-05-15 16:30:42 -0700

    Done with Misc-1

---
+++ BrokenCalculator.py
@@ -0,0 +1,12 @@
+class Solution:
+    def brokenCalc(self, startValue: int, target: int) -> int:
+        count = 0
+        while target > startValue:
+            target = target//2 if target%2==0 else target+1
+            count += 1
+        return count+startValue-target
+s = Solution()
+print(s.brokenCalc(2, 3))
+print(s.brokenCalc(5, 8))
+print(s.brokenCalc(3, 10))
+print(s.brokenCalc(1, 1000000000))
